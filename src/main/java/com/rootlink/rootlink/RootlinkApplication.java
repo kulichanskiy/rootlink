0490e2e0ea00b62a530bced65a6cb1ecffed8db8
kulichanskiy <kulichanskiy.js@gmail.com>
2026-02-12 20:37:15 -0800

    init commit

---
+++ src/main/java/com/rootlink/rootlink/RootlinkApplication.java
@@ -0,0 +1,13 @@
+package com.rootlink.rootlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rootlink/rootlink/RootlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootlink.rootlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
